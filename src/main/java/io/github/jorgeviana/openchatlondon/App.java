4047f7d994f7c61a0d34ac909be4239124fc8fb0
Jorge Viana <secoviana@gmail.com>
2019-02-22 11:09:12 +0100

    removing boilerplate created by maven archtype

--- src/main/java/io/github/jorgeviana/openchatlondon/App.java
+++ src/main/java/io/github/jorgeviana/openchatlondon/App.java
@@ -1,12 +1,8 @@
-package io.github.jorgeviana;
+package io.github.jorgeviana.openchatlondon;
 
-/**
- * Simple main application
- */
 public class App {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
-
 }
