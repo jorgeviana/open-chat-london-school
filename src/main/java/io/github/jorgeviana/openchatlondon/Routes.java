9778d5aea02bc15a609e34c98387ad0abcedd69f
Jorge Viana <secoviana@gmail.com>
2019-02-24 23:18:43 +0100

    creating the route

--- src/main/java/io/github/jorgeviana/openchatlondon/Routes.java
+++ src/main/java/io/github/jorgeviana/openchatlondon/Routes.java
@@ -1,14 +1,25 @@
 package io.github.jorgeviana.openchatlondon;
 
+import io.github.jorgeviana.openchatlondon.api.UsersAPI;
+
 import static spark.Spark.get;
+import static spark.Spark.post;
 
 public class Routes {
 
+    private UsersAPI usersAPI;
+
     public void create() {
+        createAPIs();
         openChatRoutes();
+    }
+
+    private void createAPIs() {
+        usersAPI = new UsersAPI();
     }
 
     private void openChatRoutes() {
         get("status", (req, res) -> "OpenChat: OK!");
+        post("users", (req, res) -> usersAPI.createUser(req, res));
     }
 }
---
+++ src/main/java/io/github/jorgeviana/openchatlondon/api/UsersAPI.java
@@ -0,0 +1,10 @@
+package io.github.jorgeviana.openchatlondon.api;
+
+import spark.Request;
+import spark.Response;
+
+public class UsersAPI {
+    public String createUser(Request req, Response res) {
+        throw new UnsupportedOperationException();
+    }
+}
